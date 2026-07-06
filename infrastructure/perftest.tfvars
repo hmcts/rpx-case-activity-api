77101ad348acb294fbbf62efd6da5703e8293bda
Balaji Sridharan <balaji.sridharan@hmcts.net>
2026-07-06 14:51:18 +0100

    app insights fix exui-4929

---
+++ infrastructure/perftest.tfvars
@@ -0,0 +1,3 @@
+
+# Application Insights
+sampling_percentage = 100
--- infrastructure/variables.tf
+++ infrastructure/variables.tf
@@ -39,3 +39,8 @@ variable "capacity" {
 variable "shared_product_name" {
   default = "rpx"
 }
+
+variable "sampling_percentage" {
+  default     = 1Expand commentComment on line R31Resolved
+  description = "Specifies the sampling percentage for Application Insights"
+}
